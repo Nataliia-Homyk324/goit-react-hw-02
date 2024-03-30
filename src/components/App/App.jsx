8998912b48afeeaf5678a3809dfb3fb093c7263a
Nataliia-Homyk324 <homyk324@gmail.com>
2024-03-30 22:28:20 +0200

    Description

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+import Description from '../Description/Description';
+import css from './App.module.css'
+
+
+function App() {
+    
+
+    return (
+        <section className={css.container}>
+            <Description/>
+
+        </section>
+    )
+}
+
+export default App;
---
+++ src/components/Description/Description.jsx
@@ -0,0 +1,11 @@
+import css from './Description.module.css'
+
+
+function Description() {
+    return(<>
+        <h1 className={css.mainTitle}>Sip Happens Café</h1>
+        <p className={css.mainParagraf}>Please leave your feedback about our service by selecting one of the options below.</p>
+    </>)
+    
+}
+export default Description;
